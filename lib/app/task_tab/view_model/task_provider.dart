c11348029af920d12d4c24d433bcb9e0a2685218
mac0011 <palakramani>
2024-02-13 11:44:54 +0530

    - provider changes done

--- lib/app/task_tab/view_model/task_provider.dart
+++ lib/app/task_tab/view_model/task_provider.dart
@@ -1,9 +1,10 @@
 import 'package:intl/intl.dart';
 import 'package:collection/collection.dart';
 import 'package:osm_flutter/base/base.dart';
+import '../domain/request/create_task_req_model.dart';
+import '../domain/request/get_recent_task_request_model.dart';
 import '../domain/request/search_model.dart';
 import 'package:osm_flutter/utils/utils.dart';
-import '../domain/request/create_task_req_model.dart';
 import '../domain/respones/get_create_task_response.dart';
 import '../../auth/domain/dummy/create_task_response.dart';
 import '../domain/respones/get_recent_task_response_model.dart';
@@ -51,20 +52,12 @@ class TaskProvider extends BaseNotifier implements ITaskProvider{
   late AppResponse<GetCreateTaskResponseModel> _getGetCreateTaskResponse;
   AppResponse<GetCreateTaskResponseModel> get getGetCreateTaskResponse => _getGetCreateTaskResponse;
 
-  CreateTaskReqModel createTaskReqModel = CreateTaskReqModel(
-      multipleAssignUser: [],
-      userList: [],
-      userTaskSubPointList: [],
-      docList: [],
-  );
 
   List<SearchModel> list = [];
 
   int? todayCount,comp,leave;
 
   List<SearchModel> projectUserList = [];
-  
-  bool isLoading = false;
 
   CreateTaskReqModel createTaskReqModel = CreateTaskReqModel(
     multipleAssignUser: [],
@@ -371,20 +364,6 @@ class TaskProvider extends BaseNotifier implements ITaskProvider{
     if (isLoading != null){
 
      this.isLoading = isLoading;
-
-    }
-
-    notifyListeners();
-
-    return false;
-  }
-
-
-  Future<bool?> isUpdateLoading({bool? isLoading})async{
-
-    if (isLoading != null){
-
-      this.isLoading = isLoading;
 
     }
 
